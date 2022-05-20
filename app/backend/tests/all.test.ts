8319814f5b17eff98f802c19b0bfe25d598ce3b7
guilhermecd15 <guilhermecd15@gmail.com>
2022-05-20 10:53:13 -0300

    Teste de cobertura

--- app/backend/tests/all.test.ts
+++ app/backend/tests/all.test.ts
@@ -4,7 +4,6 @@ const chaiHttp = require('chai-http');
 
 const app = require('../index');
 
-// const { Response } = require('superagent');
 const Task = require('../models/tasks');
 
 chai.use(chaiHttp);
@@ -14,10 +13,17 @@ const { expect } = chai;
 describe('Seu teste', () => {
   let chaiHttpResponse;
 
-  it('Sucesso', async () => {
+  it('Post', async () => {
     chaiHttpResponse = await chai.request(app).post('/').send({ descricao: "teste", status: "Pronto" })
 
     expect(chaiHttpResponse.status).to.be.equal(201)
     expect(chaiHttpResponse.body).to.be.keys('id', 'descricao', 'status', 'createdAt', 'updatedAt');
+  });
+
+  it('Get', async () => {
+    chaiHttpResponse = await chai.request(app).get('/')
+
+    expect(chaiHttpResponse.status).to.be.equal(200)
+    expect(chaiHttpResponse.body[0]).to.be.keys('id', 'descricao', 'status', 'createdAt', 'updatedAt');
   });
 });
